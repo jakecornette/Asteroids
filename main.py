7ab3371d0b291208fcf597e57caf3b3d03c850d9
jakecornette <jake.cornette@gmail.com>
2025-08-17 15:21:56 -0500

    "Finalized player movement"

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    
 
     while True:
         for event in pygame.event.get():
@@ -20,8 +25,10 @@ def main():
 
         screen.fill("black")
 
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+
+        for d in drawable:
+            d.draw(screen)
 
 
         pygame.display.flip()
