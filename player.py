713acde283a8b470bdd7029eb4d22155440f5702
jakecornette <jake.cornette@gmail.com>
2025-08-17 15:03:03 -0500

    "Added ability to turn"

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from circleshape import *
 from constants import *
+from main import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -18,4 +19,15 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white",self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
 
